b3407f742775ee491440e92537eeb21aafcc4295
Roshani <roshani@gmail.com>
2024-08-09 20:20:26 +0530

    login and signup

---
+++ src/app/signup/action.ts
@@ -0,0 +1,31 @@
+'use server';
+import { axiosInstance } from '@/utils/client';
+
+export const registerUser = async (userData: {
+  password: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNo: string;
+  dateOfBirth: string;
+  gender: string;
+}) => {
+  try {
+    const response = await axiosInstance.post(`/user`, {
+      password: userData.password,
+      firstName: userData.firstName,
+      lastName: userData.lastName,
+      email: userData.email,
+      phoneNo: userData.phoneNo,
+      dateOfBirth: userData.dateOfBirth,
+      gender: userData.gender,
+      
+    });
+
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log('error', error);
+    throw error;
+  }
+};
