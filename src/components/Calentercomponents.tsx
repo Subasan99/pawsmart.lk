3fa5c1a7d70e983635deb573bbb6333eafec265e
Thursika Mokanathan <thursikamokanathan@gmail.com>
2024-08-11 15:05:45 +0530

    booking ui

--- src/components/Calentercomponents.tsx
+++ src/components/Calentercomponents.tsx
@@ -1,6 +1,14 @@
 import React, { useState } from 'react';
 import { Calendar } from './ui/calendar';
 import './Scheduler.css';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 interface SchedulerProps {
   handleDateChange: (value: string) => void;
@@ -102,60 +110,85 @@ const Scheduler: React.FC<SchedulerProps> = ({ handleDateChange, name }) => {
   const timeSlots = getTimeSlots(selectedDate);
 
   return (
-    <div
-      className="flex flex-col lg:flex-row w-full lg:ml-80 container"
-      style={{
-        position: 'relative',
-        marginTop: '80px',
-      }}
-    >
-      <div
-        className="flex-1 p-6 w-full whitespace-nowrap justify-center align-center border-l-2 border-gray-200 pl-2 text-lg font-bold"
-      >
-        Scheduler
-      </div>
-      <div className="flex-2 w-full align-center justify-center mx-2">
-        <Calendar
-          className="text-black-400 hover:text-red"
-          onDayClick={onDateChange}
-        />
-      </div>
-      <div className="flex flex-col mx-4 border-gray-200 pl-2 border-r-2">
+    <div className="flex justify-center items-center container">
+    <Card className=" bg-gray-200">
+      <CardHeader>
+        <CardTitle>Select a Date & Time</CardTitle>
+        <CardDescription>When scheduling your appointment, you’ll have the flexibility to choose a date and time that fits your schedule. Our user-friendly scheduling system ensures that you can easily find available slots and secure your preferred appointment time.</CardDescription>
+      </CardHeader>
+      <CardContent className="grid grid-cols-1 lg:grid-cols-3 gap-4 ">
+        <div className="col-span-1 ">
+        <div className="flex">
+          <div
+            className="flex-3 pr-4 px-8 text-lg font-bold whitespace-nowrap"
+          >
+            Doctor
+          </div>
+          <div
+            className="flex-3 pr-4 italic text-lg text-gray-500 whitespace-nowrap"
+          >
+          fgh
+          </div>
+
+        </div>
+        </div>
+        <div className="col-span-1 flex justify-center items-center bg-white">
+          <Calendar
+            className="text-black-400 hover:text-red"
+            onDayClick={onDateChange}
+          />
+        </div>
+        <div className="col-span-1 ">
         <div className="flex">
           <div
             className="flex-3 pr-4 px-8 text-lg font-bold whitespace-nowrap"
           >
             <div>{formatDate(selectedDate)}</div>
           </div>
-          <div className="flex-1 pr-8 text-lg">
-            <div
-              className="bg-gray-400 p-1 cursor-pointer"
-              onClick={() => toggleTimeFormat('12h')}
-            >
-              12h
-            </div>
-          </div>
-          <div className="flex-1 text-lg">
-            <div
-              className="bg-gray-400 p-1 cursor-pointer"
-              onClick={() => toggleTimeFormat('24h')}
-            >
-              24h
-            </div>
-          </div>
+          <div className="flex items-center bg-white p-2 border rounded-lg">
+  <div className="flex-1 pr-4 text-lg">
+    <div
+      className=" cursor-pointer rounded"
+      onClick={() => toggleTimeFormat('12h')}
+    >
+      12h
+    </div>
+  </div>
+  <div className="flex-1 text-lg">
+    <div
+      className=" cursor-pointer rounded"
+      onClick={() => toggleTimeFormat('24h')}
+    >
+      24h
+    </div>
+  </div>
+</div>
+
         </div>
         <div className="flex flex-col mt-4">
           {timeSlots.map((slot, index) => (
             <div
               key={index}
-              className="bg-gray-200 p-2 mb-2 rounded w-full text-lg"
+              className="bg-gray-200 p-2 mb-2  ml-2 rounded w-full text-lg items-center justify-center border"
             >
               {formatTime(slot.startTime)} - {formatTime(slot.endTime)}
             </div>
           ))}
         </div>
-      </div>
-    </div>
+        </div>
+
+        
+      </CardContent>
+      <CardFooter className=" justify-end items-center">
+      <button
+          type="submit"
+          className="justify-end px-4 py-2 bg-red-500 text-white font-bold rounded hover:bg-blue-700"
+        >
+         continue
+        </button>
+      </CardFooter>
+    </Card>
+  </div>
   );
 };
 
