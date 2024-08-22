3860b29562cd7f58757ad4954636d3797ff23e11
Rushanthan Puvanendrarasa <52651698+rushanthan03@users.noreply.github.com>
2024-08-22 16:22:25 +0530

    uhu

--- src/api/route.ts
+++ src/api/route.ts
@@ -4,9 +4,9 @@ import { axiosInstance } from "@/utils/client"
 
 export async function getAllSpecializations() {
     try {
-        const response = await axiosInstance.get('/specializations');
+        const response = await axiosInstance.get('/specialization');
         return response.data;
     } catch (error) {
-        console.log('Error fetching specializations:', error)
+        console.log('Error fetching specialization:', error)
     }
 }
