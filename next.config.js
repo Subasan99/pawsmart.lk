2e5b88accd466e18b7967b528e012335dfff1e88
Rushanthan Puvanendrarasa <52651698+rushanthan03@users.noreply.github.com>
2024-09-20 21:47:29 +0530

    home

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
-
 module.exports = {
-    images: {
-      domains: ['stubby.s3.amazonaws.com'],
-    },
-    typescript: {
-      // !! WARN !!
-      // Dangerously allow production builds to successfully complete even if
-      // your project has type errors.
-      // !! WARN !!
-      ignoreBuildErrors: true,
-    },
-  }
-  
+  images: {
+    domains: ['stubby.s3.amazonaws.com'],
+  },eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+}
